53eb23a03416931edca8c1637f9009d97bd667cb
marcianaduran <cmduran.oc@gmail.com>
2024-05-05 20:14:31 -0400

    erd image and separate sql files

---
+++ EmployeeSQL/employee-sql-tables.sql
@@ -0,0 +1,41 @@
+-- DROP TABLE employees;
+
+CREATE TABLE departments(
+	dept_no VARCHAR(30) PRIMARY KEY,
+	dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE titles(
+	title_id VARCHAR(30) PRIMARY KEY,
+	title VARCHAR(30)
+);
+
+CREATE TABLE employees(
+	emp_no SERIAL PRIMARY KEY,
+	emp_title VARCHAR(30) NOT NULL,
+	FOREIGN KEY (emp_title) REFERENCES titles(title_id),
+	birth_date VARCHAR(30) NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	sex VARCHAR(30),
+	hire_date VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE dept_emp(
+	emp_no SERIAL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	dept_no VARCHAR(30) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_manager(
+	dept_no VARCHAR(30) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	emp_no INTEGER
+);
+
+CREATE TABLE salaries(
+	emp_no SERIAL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	salary INTEGER
+);
